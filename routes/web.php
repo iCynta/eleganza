e0eee7e0e1d8eaf740caae60f8386eac1789dab1
Arun Raj G S <rpv19852000@gmail.com>
2024-09-22 08:17:42 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+foreach (config('tenancy.central_domains') as $domain) {
+    Route::domain($domain)->group(function () {
+
+
+        Route::get('/', function () {
+            return view('welcome');
+        });
+
+        Route::get('/dashboard', function () {
+            return view('central.dashboard');
+        })->name('central.dashboard');
+
+        Route::get('/login', function () {
+            return view('central.dashboard');
+        })->name('central.login');
+
+    });
+}
